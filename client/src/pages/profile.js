aee9e47e05b214739db0eec1b228a9e5d162e6e3
JDBgit9 <info@jamesdbeeks.com>
2021-01-29 22:52:34 -0500

    updated profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -5,13 +5,32 @@ export class profile extends Component {
         return (
             <div>
                <div>
-                  <div>
+                  <div style={{
+                   display: "left",
+                   justifyContent:"space-around",
+                   margin:"18px 0px"
+               }}>
                     <img style={{width: "160px", borderRadius:"80px", marginTop: "60px"}}src="https://images.unsplash.com/photo-1558216144-fef86b75da36?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTEzfHxwZXJzb258ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
-                    <h4>Username</h4>
                    </div>
-                  <div>
-                    
-                  </div>
+                  <div style={{
+                      marginTop: "50px"
+                  }}>
+                      <div style={{
+                          display:"right",
+                          justifyContent:"left",
+                          alignItems: "center", 
+                          }}>
+                          <h3>UserName Goes Here</h3>
+                      <div>
+                          <h5>Points 100</h5>
+                          <h5>Debates 0</h5> 
+                          <h5>Wins 0</h5>
+                          <h5>Losses 0</h5>
+                          <h5>Memebaters 0</h5>
+                          <h5>Following 0</h5>
+                       </div>
+                      </div>
+                  </div >
                </div>
             </div>
         )
