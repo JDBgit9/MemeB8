ec8102c9766bd740a2d67e60ba8b26414bbc6d9c
JDBgit9 <info@jamesdbeeks.com>
2021-01-29 22:29:10 -0500

    added pic and username to profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,7 +4,15 @@ export class profile extends Component {
     render() {
         return (
             <div>
-                <h1>Profile Page</h1>
+               <div>
+                  <div>
+                    <img style={{width: "160px", borderRadius:"80px", marginTop: "60px"}}src="https://images.unsplash.com/photo-1558216144-fef86b75da36?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTEzfHxwZXJzb258ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
+                    <h4>Username</h4>
+                   </div>
+                  <div>
+                    
+                  </div>
+               </div>
             </div>
         )
     }
