04f8b8120f050439af88e6c72c64e86a8625fbab
JDBgit9 <info@jamesdbeeks.com>
2021-02-01 12:04:17 -0500

    updating the membate section wu=it useeffect and connecting to server

--- client/src/components/Memebate.js
+++ client/src/components/Memebate.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios";
 
 function Memebate() {
+    let {id}=useParams();
+    const [loading, setLoading]=useState(false)
+    const [isError, setIsError]=useState(false)
+    const [data, setData]=useState()
+    useEffect(()=> {
+        let ignore = false;
+        async function fetchData() {
+            setLoading(true);
+            setIsError(false);
+            try {
+              const resp = await axios.get("/media", {body:{id:id}});
+              if (!ignore) setData(resp.data);
+            } catch (error) {
+              console.error(error);
+              setIsError(true);
+            }
+            setLoading(false);
+          }
+      console.log(data)
+          fetchData();
+          return () => {
+            ignore = true;
+          };
+    }, [id])
+
     return (
         <div className="memebate">
-            
+            {id}
         </div>
     )
 }
