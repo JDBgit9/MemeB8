e49a01b02d1f9b1d4d22e0354f02bd238f9850a0
JDBgit9 <info@jamesdbeeks.com>
2021-02-10 16:17:56 -0500

    added handlse submit to button of meem

--- client/src/components/MemeBuilder.js
+++ client/src/components/MemeBuilder.js
@@ -3,14 +3,17 @@ import { useHistory } from "react-router-dom";
 import "./Memebuilder.css";
 
 
-  function MemeBuilder() {
+function MemeBuilder() {
   const [meme, setMeme] = useState([]);
-  const [defaultMemes, setDefaultMemes]=useState([])
+  const [defaultMemes, setDefaultMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
   const [captions, setCaptions] = useState([]);
   const api = "384be08c76d654f4105db56ec7dd11";
-
+  const [defaultImage, setDefaultImage]=useState("");
   const history = useHistory();
+  const [showForm, setShowForm]=useState(false);
+  const [memeTopText, setMemeTopText]=useState("");
+  const [memeBottomText, setMemeBottomText]=useState("");
 
   const updateCaption = (e, index) => {
     const text = e.target.value || "";
@@ -30,7 +33,7 @@ import "./Memebuilder.css";
     const formData = new FormData();
     formData.append("template_id", currentMeme.id);
     captions.forEach((c, index) => formData.append(`boxes[${index}][text]`, c));
-  }
+  };
   const shuffleMeme = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i);
@@ -39,17 +42,33 @@ import "./Memebuilder.css";
       array[j] = temp;
     }
   };
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  let memeObject={
+    topText: memeTopText,
+    bottomText: memeBottomText,
+    imgUrl: `https://s3-us-west-2.amazonaws.com/memebuilder/default/${defaultImage}.jpg`
 
+  }
+  console.log(memeObject)
+  fetch("http://memebuild.com/api/1.0/generateMeme", {
+    method: "POST",
+    body: JSON.stringify(memeObject)
+  }).then(response=>{console.log(response.json())})
+}
   useEffect(() => {
     fetch("/default-memes")
-    .then(response=>{return response.json()})
+      .then((response) => {
+        return response.json();
+      })
       .then((res) => {
         const _meme = res;
         console.log(res);
         setDefaultMemes(res);
-      }).catch(error=>{
-        console.log(error)
       })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   useEffect(() => {
@@ -57,7 +76,13 @@ import "./Memebuilder.css";
       setCaptions(Array(meme[memeIndex].box_count).fill(""));
     }
   }, [memeIndex, meme]);
-  return defaultMemes.length > 0? (
+  const handleMemeClick = (file)=>{
+    setDefaultImage(file)
+    if (!showForm){
+    setShowForm(true)
+    }
+  }
+  return defaultMemes.length > 0 ? (
     <div className="container">
       {/* <button onClick={generateMeme} className="generate">
         Generate
@@ -73,18 +98,31 @@ import "./Memebuilder.css";
       ))}
       <img alt="meme" src={meme[memeIndex].url} /> */}
       <div className="imageselector">
-          {
-              defaultMemes?.map((dmeme, index)=>{
-                return(
-                    <img src={`https://s3-us-west-2.amazonaws.com/memebuilder/default/${dmeme.file}.jpg`} alt={dmeme.name}/>
-                )
-              })
-          }
+        {defaultMemes?.map((dmeme, index) => {
+          return (
+            <img
+              src={`https://s3-us-west-2.amazonaws.com/memebuilder/default/${dmeme.file}.jpg`}
+              alt={dmeme.name}
+              onClick={() => handleMemeClick(dmeme.file)}
+            />
+          );
+        })}
       </div>
+      {
+        showForm && 
+        (
+          <div className="meme-form">
+            <h2>step 2: enter meme text</h2>
+            <input type="text" value={memeTopText} placeholder="enter text for top of meme" onChange={(e)=>setMemeTopText(e.target.value)}/>
+            <input type="text" value={memeBottomText} placeholder="enter text for bottom of meme" onChange={(e)=>setMemeBottomText(e.target.value)}/>
+          <button onClick={(e)=>handleSubmit(e)}>Post Meme</button>
+          </div>
+        )
+      }
     </div>
   ) : (
     <></>
   );
 }
 
-export default MemeBuilder
+export default MemeBuilder;
