2c76ee6c47d6ee7af0fdaea5b77a25c51651fb02
JDBgit9 <info@jamesdbeeks.com>
2021-02-14 21:44:24 -0500

    updated model and challenge forms

---
+++ client/src/components/ChallengeForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChallengeForm() {
+    return (
+        <div className="challengeform">
+            
+        </div>
+    )
+}
+
+export default ChallengeForm
