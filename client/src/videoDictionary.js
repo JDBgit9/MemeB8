dc3e9dfc896d53039139587bdcec303441d0b69f
JDBgit9 <info@jamesdbeeks.com>
2021-02-19 21:17:02 -0500

    fixed the rendering of the bitchute challenge media

--- client/src/videoDictionary.js
+++ client/src/videoDictionary.js
@@ -17,10 +17,12 @@ function formatUrl(url, provider){
 if (provider==="youtube"){
   let id =url.split("=")[1]
   return `https://www.youtube.com/embed/${id}`
-} else if(provider==="rumble" || provider==="bitchute"){
+} else if(provider==="rumble"){
     return url
 }else if (provider==="vimeo"){
     let id =url.replace("https://", "").split("/")[1]
     return `https://player.vimeo.com/video/${id}`
+}else if (provider ==="bitchute"){
+  return url.replace("video", "embed")
 }
 }
