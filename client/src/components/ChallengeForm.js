2393c270df5c11f3b74c2532af243e7dd9347fe1
JDBgit9 <jamesthejust99@outlook.com>
2022-04-25 08:13:09 -0400

    merging and stanging alot of changes

---
+++ client/src/components/ChallengeForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import "./ChallengeForm.css";
+
+
+function ChallengeForm({media_id}) {
+  const [showForm, setShowForm] = useState(false);
+  const [challenge, setChallenge] = useState("");
+  const [sources, setSources] = useState("");
+  const [tags, setTags] = useState("");
+  const user = useAuth0();
+  const handleSubmit = async(e)=>{
+    let userInfo=await axios.get(`/users/${user.user.email}`)
+    let object = {
+      challenge: challenge,
+      likes: 0,
+      dislikes: 0,
+      sources: sources.split(",").map(source=>source),
+      tags: tags.split(",").map(tag=>tag),
+      media_id: media_id,
+      user: {
+        id: userInfo.data[0]._id,
+        userName: userInfo.data[0].username 
+      }
+    }
+    console.log(user)
+    console.log(object)
+    console.log(userInfo)
+    let submit = await axios.post("/challenge", {data: object}, 
+    { headers: {"Content-Type": "application/json"},}
+    )
+  }
+
+
+  return (
+    <div className="challengeform">
+      {!showForm ? (
+        <button onClick={()=>setShowForm(true)}>Challenge This</button>
+      ) : (
+          <>
+        <input
+          type="text"
+          value={challenge}
+          onChange={(e) => setChallenge(e.target.value)}
+          placeholder="Enter Challenge Url"
+        />
+        <textarea value={sources}onChange={(e) => setSources(e.target.value)}
+        placeholder="Enter Sources seperated by commas Url's"></textarea>
+        <textarea value={tags}onChange={(e) => setTags(e.target.value)}
+        placeholder="Enter Tags seperated by commas"></textarea>
+        <button onClick={(e)=>handleSubmit(e)}>Submit</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ChallengeForm;
