a2694411a9a0164561261c5f38fccc1cc5355a25
JDBgit9 <jamesthejust99@outlook.com>
2022-04-26 23:43:13 -0400

    added asynce function getmedia

--- client/src/components/userMedia.js
+++ client/src/components/userMedia.js
@@ -4,6 +4,17 @@ import React from 'react';
 const obj = {hello: 'world'};
 const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
 
+async function getMedia(constraints) {
+    let stream = null;
+  
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      /* use the stream */
+    } catch(err) {
+      /* handle the error */
+    }
+  }
+
 start()
 start(timeslice)
   
