e5caf85716b974e707f59f0b11a74c06ba7ad679
JDBgit9 <jamesthejust99@outlook.com>
2022-04-27 15:45:21 -0400

    import mic recorder

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import User from "./User";
 import "./Post.css";
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
