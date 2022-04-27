61b63103597c4f4a9e12721e69f16345218eeab4
JDBgit9 <jamesthejust99@outlook.com>
2022-04-26 23:34:54 -0400

    added a blob and start functio in usermedia

--- client/src/components/userMedia.js
+++ client/src/components/userMedia.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+
+const obj = {hello: 'world'};
+const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
+  record.onclick = function() {
+    mediaRecorder.start();
+    console.log("recorder started");
+  }
+
+
