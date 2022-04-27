f3ba8e725ba9668bf54c15e16f988c55d96e78e9
JDBgit9 <jamesthejust99@outlook.com>
2022-04-26 23:36:46 -0400

    added a start function

--- client/src/components/userMedia.js
+++ client/src/components/userMedia.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 const obj = {hello: 'world'};
 const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
-  record.onclick = function() {
+
+start()
+start(timeslice)
+  
+record.onclick = function() {
     mediaRecorder.start();
     console.log("recorder started");
   }
