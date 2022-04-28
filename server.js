dab551a562859050ea66988d3bb225656104fb38
JDBgit9 <jamesthejust99@outlook.com>
2022-04-28 17:28:56 -0400

    moved port in server

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, function (error) {
   if (error) console.log("listen error", error);
